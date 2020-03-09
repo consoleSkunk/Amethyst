69f624fac34b55e26661f81592693ae499bbb3fa
Erika <50500602+ErikaIsGirl@users.noreply.github.com>
2020-03-09 12:49:10 -0600

    Bring back the Random Comic Generator

---
+++ commands/rcg.js
@@ -0,0 +1,47 @@
+var qs = require("querystring"),
+    Discord = require("discord.js"),
+    request = require("request"),
+    name = require('../package.json').name,
+    version = require('../package.json').version;
+
+exports.module = {
+	commands: ["rcg"],
+	description: "Returns a random comic from Explosm.net's [Random Comic Generator](http://explosm.net/rcg).\nPanel locking is currently unsupported.",
+	syntax: null,
+	tags: [],
+	permissions: [],
+	process: function(client, msg, params) {
+		msg.channel.startTyping();
+		var options = {
+			url: "http://explosm.net/rcg/view/",
+			headers: {
+				'User-Agent': `${name}/${version}`
+			}
+		};
+		request(options,
+		function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+				if (body) {
+					var regex = /https:\/\/rcg-cdn\.explosm\.net\/comics\/[0-9A-F]{12}\/[0-9A-F]{12}\/[0-9A-F]{12}\/([a-z]{9})\.png/;
+					var RCG = regex.exec(body);
+					if(!msg.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
+						msg.reply('http://explosm.net/rcg/' + RCG[1]);
+					} else {
+						msg.reply(undefined, {embed:{
+							title: "Random Comic Generator",
+							url: 'http://explosm.net/rcg/' + RCG[1],
+							image: {
+								url: RCG[0]
+							}
+						}});
+					}
+				}
+			}
+			else {
+				msg.reply(`${response.statusCode} ${response.statusMessage}`);
+			}
+		});
+		msg.channel.stopTyping();
+	}
+};
