12053e321cd03448c80b25abd50d525085b00ace
Erika <50500602+Enbika@users.noreply.github.com>
2020-03-31 17:15:11 -0600

    Clean up /avatar code and fix /avatar [ID]

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,50 +1,40 @@
 exports.module = {
 	commands: ["avatar","avie","av"],
 	description: "Retrieves the mentioned user's avatar, or yours if not specified.",
-	syntax: "[@mention]",
+	syntax: "[@mention or user ID]",
 	tags: [],
 	process: function(client, msg, params){
+		function displayAvatar(user) {
+			if(user.avatar != null) {
+				if(!msg.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
+					msg.reply(user.displayAvatarURL({size:2048}).replace(".webp",".png"));
+				} else {
+					msg.reply(undefined, {embed: {
+						author: {
+							name: `${user.tag}'s avatar`
+						},
+						image: {
+							url: user.displayAvatarURL({size:2048}).replace(".webp",".png")
+						},
+						url: user.displayAvatarURL({size:2048}).replace(".webp",".png"),
+						color: 16426522
+					}});
+				}
+			} else {
+				msg.reply(`**${user.tag}** does not have an avatar.`);
+			}
+		}
+
 		if (params.length > 0) {
 			if (msg.mentions.users.array()[0] != null) {
 				var mention = msg.mentions.users.array()[0];
-				if(mention.avatar != null) {
-					if(!msg.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
-						msg.reply(mention.displayAvatarURL({size:2048}).replace(".webp",".png"));
-					} else {
-						msg.reply(undefined, {embed: {
-							author: {
-								name: `${mention.tag}'s avatar`
-							},
-							image: {
-								url: mention.displayAvatarURL({size:2048}).replace(".webp",".png")
-							},
-							url: mention.displayAvatarURL({size:2048}).replace(".webp",".png"),
-							color: 16426522
-						}});
-					}
-				} else {
-					msg.reply(`**${mention.username}** does not have an avatar.`);
-				}
-			} else if (/^\d+$/.test(params)) {
-				if(client.users.get(params) !== undefined) {
-					var user = client.users.get(params);
-					if(!msg.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
-						msg.reply(user.displayAvatarURL({size:2048}).replace(".webp",".png"));
-					} else {
-						msg.reply(undefined, {embed: {
-							author: {
-								name: `${user.tag}'s avatar`
-							},
-							image: {
-								url: user.displayAvatarURL({size:2048}).replace(".webp",".png")
-							},
-							url: user.displayAvatarURL({size:2048}).replace(".webp",".png"),
-							color: 16426522
-						}});
-					}
-				} else {
-					msg.reply("I could not find the user you requested.");
-				}
+				displayAvatar(mention);
+			} else if (/^\d+$/.test(params) && client.users.cache.get(params) !== undefined) {
+				client.users.fetch(params).then(user => {
+					displayAvatar(user);
+				});
+			} else {
+				msg.reply("I could not find the user you requested.");
 			}
 		}
 		else {
