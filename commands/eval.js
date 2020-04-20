b21bf99b460ee4cd4fa05799a6e0c8558a461d49
Erika <50500602+Enbika@users.noreply.github.com>
2020-04-20 13:09:07 -0600

    Remove unnecessary depth from eval response (additionally, clean up the eval code)

--- commands/eval.js
+++ commands/eval.js
@@ -10,16 +10,14 @@ exports.module = {
 		if(params) {
 			try {
 				var response = eval(params);
-				var respString = util.format(response).replace(/(M[a-zA-Z0-9]{23}\.[a-zA-Z0-9_\-]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84})/g,"{TOKEN REMOVED}");
+				var respString = util.inspect(response,{depth:0}).replace(/([a-zA-Z0-9]{24}\.[a-zA-Z0-9_\-]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84})/g,"{TOKEN REMOVED}");
 
-				if(typeof response !== "undefined" && typeof response !== "object") {
-					msg.reply(respString,{code:true});
-				} else if(typeof response == "object" && !respString.startsWith("Promise")) {
-					if(respString.length <= 1991) {
+				if(respString !== ("Promise { <pending> }")) { // hide unresolved Promise responses
+					if(respString.length <= 1991) { // taking into account the markdown required for the code block
 						msg.reply(respString,{code:"js"});
 					} else {
 						if(msg.channel.permissionsFor(client.user).has("ATTACH_FILES")) {
-							msg.reply('',{files:[{attachment:Buffer.from(respString),name:`${params}.txt`}]});
+							msg.reply('',{files:[{attachment:Buffer.from(respString),name:"message.txt"}]});
 						} else {
 							msg.reply(respString.substring(0,1976) + "…\n\n[truncated]",{code:"js"});
 						}
