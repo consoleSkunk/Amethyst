84b1c043925969dbeaca6d86687237abe832fe5a
Erika <50500602+Enbika@users.noreply.github.com>
2020-07-23 18:51:13 -0600

    /sdate: Allow setting epoch date

--- commands/sdate.js
+++ commands/sdate.js
@@ -1,17 +1,26 @@
 exports.module = {
 	commands: ["sdate","september"],
 	description: "Check what day of [Eternal September](https://en.wikipedia.org/wiki/Eternal_September) it is.",
-	syntax: null,
+	syntax: "[YYYY-MM]",
 	tags: [],
-	process: function(client, msg){
+	process: function(client, msg, params) {
+		var epochRegex = /^(\d{4})-(\d{2})$/
+		if(!epochRegex.test(params))
+			params = "1993-09";
+		
+		var epoch = epochRegex.exec(params);
+
 		var today = new Date();
-		var september = new Date(1993,8,1);
+		var epochDate = new Date(epoch[1],epoch[2]-1,1);
+		if(epochDate > today)
+			epochDate = new Date(today.getFullYear(),today.getMonth(),1);
 		var dayDiff = Math.ceil(
 			Math.abs(
-				today.getTime() - september.getTime()
+				today.getTime() - epochDate.getTime()
 			) / (1000 * 3600 *24)
 		);
+		var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 		var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-		msg.reply(`Today is ${days[today.getDay()]}, September ${dayDiff}, 1993`);
+		msg.reply(`Today is ${days[today.getDay()]}, ${months[epochDate.getMonth()]} ${dayDiff}, ${epochDate.getFullYear()}`);
 	}
 };
