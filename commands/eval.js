8fcbc6b1707ecfe7f1a050b6a240822c402ae660
Erika <50500602+Enbika@users.noreply.github.com>
2021-04-03 19:01:29 -0600

    Adjust eval command for inline text attachments

--- commands/eval.js
+++ commands/eval.js
@@ -11,14 +11,14 @@ exports.module = {
 		if(params) {
 			try {
 				var response = eval(params);
-				var respString = util.inspect(response,{depth:0}).replace(/([a-zA-Z0-9]{24}\.[a-zA-Z0-9_\-]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84})/g,"{TOKEN REMOVED}");
+				var respString = util.inspect(response).replace(/([a-zA-Z0-9]{24}\.[a-zA-Z0-9_\-]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84})/g,"{TOKEN REMOVED}");
 
 				if(typeof response !== "undefined" && respString !== ("Promise { <pending> }")) { // hide Promise responses
 					if(respString.length <= 1991) { // taking into account the markdown required for the code block
 						msg.reply(respString,{code:"js"});
 					} else {
 						if(msg.channel.permissionsFor(client.user).has("ATTACH_FILES")) {
-							msg.reply('',{files:[{attachment:Buffer.from(respString),name:"message.txt"}]});
+							msg.reply('',{files:[{attachment:Buffer.from(respString),name:"eval.js"}]});
 						} else {
 							msg.reply(respString.substring(0,1976) + "…\n\n[truncated]",{code:"js"});
 						}
