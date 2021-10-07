e6dd7d236b9ee381c700185127327c1d7beb8e09
Erika <50500602+Enbika@users.noreply.github.com>
2021-10-07 17:46:43 -0600

    Implement server avatars

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,10 +8,32 @@ exports.module = {
 		type: 'USER',
 		description: "The user you want an avatar from",
 		required: false
+	},
+	{
+		name: 'server',
+		type: 'BOOLEAN',
+		description: "Whether to return the server avatar",
+		required: false
 	}],
 	process: function(interaction) {
 		function displayAvatar(user) {
-			if(user.avatar != null) {
+			if(interaction.options.getBoolean('server') && user.avatar !== null && interaction.guild.members.resolve(user) != null) {
+				var hasServerAvatar = (interaction.guild.members.resolve(user).avatar != null)
+				var serverAvatar = interaction.guild.members.resolve(user).displayAvatarURL({size:4096,format:"png",dynamic:true});
+				var globalAvatar = user.displayAvatarURL({size:4096,format:"png",dynamic:true});
+				
+				interaction.reply({embeds: [new MessageEmbed({
+					author: {
+						name: `${user.tag}'s ${hasServerAvatar ? "server " : ""}avatar`,
+						iconURL: (hasServerAvatar ? globalAvatar : user.defaultAvatarURL)
+					},
+					image: {
+						url: (hasServerAvatar ? serverAvatar : globalAvatar)
+					},
+					url: (hasServerAvatar ? serverAvatar : globalAvatar),
+					color: [0x5865f2,0x757e8a,0x3ba55c,0xfaa61a,0xed4245][user.discriminator % 5]
+				})]});
+			} else if(user.avatar != null) {
 				interaction.reply({embeds: [new MessageEmbed({
 					author: {
 						name: `${user.tag}'s avatar`,
