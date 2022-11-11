8390e4fa3f927b4d54445a9aa03de740bbf8af51
Erika <50500602+Enbika@users.noreply.github.com>
2022-11-10 17:41:58 -0700

    make /avatar require a user

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,7 +8,7 @@ exports.module = {
 			name: 'user',
 			type: ApplicationCommandOptionType.User,
 			description: "The user you want an avatar from",
-			required: false
+			required: true
 		},
 		{
 			name: 'server',
@@ -18,46 +18,38 @@ exports.module = {
 		}],
 	},
 	process: function(interaction) {
-		function displayAvatar(user) {
-			if(interaction.options.getBoolean('server') && user.avatar !== null && interaction.guild.members.resolve(user) != null) {
-				var hasServerAvatar = (interaction.guild.members.resolve(user).avatar != null)
-				var serverAvatar = interaction.guild.members.resolve(user).displayAvatarURL({size:4096,format:"png",dynamic:true});
-				var globalAvatar = user.displayAvatarURL({size:4096,format:"png",dynamic:true});
-				
-				interaction.reply({embeds: [new EmbedBuilder({
-					author: {
-						name: `${user.tag}'s ${hasServerAvatar ? "server " : ""}avatar`,
-						iconURL: (hasServerAvatar ? globalAvatar : user.defaultAvatarURL)
-					},
-					image: {
-						url: (hasServerAvatar ? serverAvatar : globalAvatar)
-					},
-					url: (hasServerAvatar ? serverAvatar : globalAvatar),
-					color: [0x5865f2,0x757e8a,0x3ba55c,0xfaa61a,0xed4245][user.discriminator % 5]
-				})]});
-			} else if(user.avatar != null) {
-				interaction.reply({embeds: [new EmbedBuilder({
-					author: {
-						name: `${user.tag}'s avatar`,
-						iconURL: user.defaultAvatarURL
-					},
-					image: {
-						url: user.displayAvatarURL({size:4096,format:"png",dynamic:true})
-					},
-					url: user.displayAvatarURL({size:4096,format:"png",dynamic:true}),
-					color: [0x5865f2,0x757e8a,0x3ba55c,0xfaa61a,0xed4245][user.discriminator % 5]
-				})]});
-			} else {
-				interaction.reply({content: `**${user.tag}** does not have an avatar.`, ephemeral: true});
-			}
-		}
-
 		var user = interaction.options.getUser('user');
 
-		if (user !== null) {
-			displayAvatar(user);
+		if(interaction.options.getBoolean('server') && user.avatar !== null && interaction.guild.members.resolve(user) != null) {
+			var hasServerAvatar = (interaction.guild.members.resolve(user).avatar != null)
+			var serverAvatar = interaction.guild.members.resolve(user).displayAvatarURL({size:4096,format:"png",dynamic:true});
+			var globalAvatar = user.displayAvatarURL({size:4096,format:"png",dynamic:true});
+			
+			interaction.reply({embeds: [new EmbedBuilder({
+				author: {
+					name: `${user.tag}'s ${hasServerAvatar ? "server " : ""}avatar`,
+					iconURL: (hasServerAvatar ? globalAvatar : user.defaultAvatarURL)
+				},
+				image: {
+					url: (hasServerAvatar ? serverAvatar : globalAvatar)
+				},
+				url: (hasServerAvatar ? serverAvatar : globalAvatar),
+				color: [0x5865f2,0x757e8a,0x3ba55c,0xfaa61a,0xed4245][user.discriminator % 5]
+			})]});
+		} else if(user.avatar != null) {
+			interaction.reply({embeds: [new EmbedBuilder({
+				author: {
+					name: `${user.tag}'s avatar`,
+					iconURL: user.defaultAvatarURL
+				},
+				image: {
+					url: user.displayAvatarURL({size:4096,format:"png",dynamic:true})
+				},
+				url: user.displayAvatarURL({size:4096,format:"png",dynamic:true}),
+				color: [0x5865f2,0x757e8a,0x3ba55c,0xfaa61a,0xed4245][user.discriminator % 5]
+			})]});
 		} else {
-			displayAvatar(interaction.user);
+			interaction.reply({content: `**${user.tag}** does not have an avatar.`, ephemeral: true});
 		}
 	}
 };
