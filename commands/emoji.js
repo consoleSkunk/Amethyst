4913cdb5592b98489cf5688b5ce66d734f5022ab
Erika <50500602+Enbika@users.noreply.github.com>
2023-04-04 17:14:05 -0600

    remove emoji list

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,94 +3,54 @@ var Discord = require("discord.js");
 exports.module = {
 	command: {
 		name: "emoji",
-		description: "View the details of a custom emoji, or list the custom emojis in this server.",
+		description: "View the details of a custom emoji.",
 		options: [{
 			name: 'emoji',
 			type: Discord.ApplicationCommandOptionType.String,
 			description: "The emoji to fetch.",
-			required: false
+			required: true
 		}],
 	},
 	process: function(interaction, client) {
 		var option = interaction.options.getString('emoji');
 
-		if (option !== null) {
+		var emojiRegex = /^<(a?):(\w+):(\d+)>$/;
+		var idRegex = /^\d+$/;
 
-			var emojiRegex = /^<(a?):(\w+):(\d+)>$/;
-			var idRegex = /^\d+$/;
-
-			if(option.match(emojiRegex)) {
-				var regex = emojiRegex.exec(option),
-					emoji = client.emojis.resolve(regex[3]),
-					embed = new Discord.EmbedBuilder({
-					title: `${emoji !== null ? (emoji.requiresColons ? "\\:"+emoji.name+"\\:" : emoji.name) : "\\:" + regex[2] + "\\:"}`,
-					image: {
-						url: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}?v=1`
-					},
-					url: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}?v=1`,
-					timestamp: Discord.SnowflakeUtil.deconstruct(regex[3]).date,
-					color: 16426522
-				});
-				if(emoji !== null) {
-					embed.setFooter({text: emoji.guild.name, iconURL: emoji.guild.iconURL({size:128,format:"png",dynamic:true})});
-				}
-				interaction.reply({content: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}`, embeds: [embed]});
-			} else if(idRegex.test(option)) {
-				var id = option;
-					emoji = client.emojis.resolve(id),
-					embed = new Discord.EmbedBuilder({
-					image: {
-						url: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}?v=1`
-					},
-					url: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}?v=1`,
-					timestamp: Discord.SnowflakeUtil.deconstruct(id).date,
-					color: 16426522
-				});
-				if(emoji !== null) {
-					embed.setTitle(emoji.requiresColons ? ":"+emoji.name+":" : emoji.name);
-					embed.setFooter({text: emoji.guild.name, iconURL: emoji.guild.iconURL({size:128,format:"png",dynamic:true})});
-				}
-				interaction.reply({content: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}`, embeds: [embed]});
-			} else {
-				interaction.reply({content: "No valid custom emoji was specified.",ephemeral: true});
-			}
-		} else {
-			let emojis = interaction.guild.emojis.cache,
-			    static = emojis.filter(emoji => !emoji.animated),
-			    animated = emojis.filter(emoji => emoji.animated),emojiList = "",
-			    numAnimated = 0,
-			    numStatic = 0,
-			    i = 0,
-			    fields = [];
-
-				emojis.each((emoji) => {
-					if(emoji.animated && !emoji.managed) numAnimated++; else numStatic++;
-	
-					var boostLevel = 
-						(emoji.animated && numAnimated > 150) || (!emoji.animated && numStatic > 150) ? " (Lv 3)" :
-						(emoji.animated && numAnimated > 100) || (!emoji.animated && numStatic > 100) ? " (Lv 2)" :
-						(emoji.animated && numAnimated > 50) || (!emoji.animated && numStatic > 50) ? " (Lv 1)" : "";
-	
-					emojiList += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`:${emoji.name}:\`${boostLevel}\n`
-					if ((i+1) % 10 === 0 || i+1 == emojis.size){
-						fields.push({name: "\u200B",value: emojiList,inline: true});
-						emojiList = "";
-					}
-					i++;
-				});
-			interaction.reply({embeds: [new Discord.EmbedBuilder({
-				author: {
-					name: interaction.guild.name,
-					iconURL: interaction.guild.iconURL({size:2048}).replace(".webp",".png")
+		if(option.match(emojiRegex)) {
+			var regex = emojiRegex.exec(option),
+				emoji = client.emojis.resolve(regex[3]),
+				embed = new Discord.EmbedBuilder({
+				title: `${emoji !== null ? (emoji.requiresColons ? "\\:"+emoji.name+"\\:" : emoji.name) : "\\:" + regex[2] + "\\:"}`,
+				image: {
+					url: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}?v=1`
 				},
-				title: "Emoji List",
-				color: 16426522,
-				fields: fields,
-				footer: {
-					text: `${emojis.size} total: ${static.size} static, ${animated.size} animated`
-				}
-			})]});
+				url: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}?v=1`,
+				timestamp: Discord.SnowflakeUtil.deconstruct(regex[3]).date,
+				color: 16426522
+			});
+			if(emoji !== null) {
+				embed.setFooter({text: emoji.guild.name, iconURL: emoji.guild.iconURL({size:128,format:"png",dynamic:true})});
+			}
+			interaction.reply({content: `https://cdn.discordapp.com/emojis/${regex[3]}.${regex[1] == "a" ? "gif" : "png"}`, embeds: [embed]});
+		} else if(idRegex.test(option)) {
+			var id = option;
+				emoji = client.emojis.resolve(id),
+				embed = new Discord.EmbedBuilder({
+				image: {
+					url: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}?v=1`
+				},
+				url: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}?v=1`,
+				timestamp: Discord.SnowflakeUtil.deconstruct(id).date,
+				color: 16426522
+			});
+			if(emoji !== null) {
+				embed.setTitle(emoji.requiresColons ? ":"+emoji.name+":" : emoji.name);
+				embed.setFooter({text: emoji.guild.name, iconURL: emoji.guild.iconURL({size:128,format:"png",dynamic:true})});
+			}
+			interaction.reply({content: `https://cdn.discordapp.com/emojis/${id}.${emoji !== null && emoji.animated ? "gif" : "png"}`, embeds: [embed]});
+		} else {
+			interaction.reply({content: "No valid custom emoji was specified.",ephemeral: true});
 		}
-
 	}
 };
