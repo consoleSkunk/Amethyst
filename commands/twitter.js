716bc42874a7fc91ed6ee3f2582ed250f1c7c2dd
Erika <50500602+Enbika@users.noreply.github.com>
2023-04-28 12:38:34 -0600

    Rewrite /twitter to use FixTweet API

--- commands/twitter.js
+++ commands/twitter.js
@@ -1,13 +1,7 @@
 var { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js'),
-    { twitter } = require('../config/config.json');
-    TwitterClient = require('twitter');
-
-	var client = new TwitterClient({
-		consumer_key: twitter.consumer_key,
-		consumer_secret: twitter.consumer_secret,
-		bearer_token: twitter.bearer_token
-	});
-
+    { contact } = require('../config/config.json'),
+    { name, version } = require('../package.json'),
+    fetch = require("node-fetch");
 
 exports.module = {
 	command: {
@@ -30,131 +24,127 @@ exports.module = {
 		
 		var tweetId = regex.exec(interaction.options.getString('url'))[1];
 		
-		client.get('statuses/show', {id: tweetId, tweet_mode: 'extended'}, (error, tweet) => {
-			if(error) {
-				interaction.reply({content: error[0].message, ephemeral: true})
-			} else {
-				function parseTweet(thisTweet) {
-					var parsedText = thisTweet.full_text;
-					// replace each hashtag with a link to it
-					thisTweet.entities.hashtags.forEach((hashtag) => {
-						parsedText = parsedText.replace(`#${hashtag.text}`, `[#\u2060${hashtag.text}](https://twitter.com/hashtag/${encodeURIComponent(hashtag.text)})`)
-					})
-					
-					if(thisTweet.entities.media)
-						thisTweet.entities.media.forEach((media) => {
-							// strip out media link *unless* we're in a QRT (as we do not show quoted media)
-							parsedText = parsedText.replace(media.url,
-								(thisTweet === tweet.quoted_status ? `[${media.display_url}](${media.expanded_url})` : "")
-							)
-						})
-
-					//cashtags are pretty much the same as hashtags, except it needs to be a search result
-					thisTweet.entities.symbols.forEach((symbol) => {
-						parsedText = parsedText.replace(`\$${symbol.text}`, `[\$\u2060${symbol.text}](https://twitter.com/search?q=%24${encodeURIComponent(symbol.text)})`)
-					})
-
-					// t.co links are replaced with their original links unless it's too long
-					thisTweet.entities.urls.forEach((url) => {
-						parsedText = parsedText.replace(url.url, `[${url.display_url}](${url.expanded_url.length <= 800/thisTweet.entities.urls.length ? url.expanded_url : url.url})`)
-					})
-
-					// mentions require a case-insensitive search in case there's different capitalization
-					thisTweet.entities.user_mentions.forEach((mention) => {
-						parsedText = parsedText.replace(new RegExp(`@(${mention.screen_name})`,'i'), `[@\u2060$1](https://twitter.com/${encodeURIComponent(mention.screen_name)})`)
-					})
-				
-					// escape encoded html
-					parsedText = parsedText.replaceAll("&lt;","<");
-					parsedText = parsedText.replaceAll("&gt;",">");
-					parsedText = parsedText.replaceAll("&amp;","&");
-					return parsedText;
-				}
-
-				var content = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
-
-				var embeds = [
-					new EmbedBuilder({
-						author: {
-							name: `${tweet.user.name} (@${tweet.user.screen_name})`,
-							url: `https://twitter.com/${tweet.user.screen_name}`,
-							iconURL: tweet.user.profile_image_url_https
-						},
-						url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-						color: tweet.possibly_sensitive ? 0xf4212e : 0x00ba7c,
-						description: parseTweet(tweet),
-						footer: {
-							text: /<a .+>(.+)<\/a>/.exec(tweet.source)[1],
-							iconURL: "https://abs.twimg.com/icons/apple-touch-icon-192x192.png"
-						},
-						timestamp: new Date(tweet.created_at).toISOString()
-					})
-				];
-				if(tweet.user.verified) {
-					// change twitter logo in footer to verified badge
-					embeds[0].setFooter({
-						text: /<a .+>(.+)<\/a>/.exec(tweet.source)[1],
-						iconURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/192px-Twitter_Verified_Badge.svg.png"
-					});
-				}
-
-				if(tweet.quoted_status)
-					embeds[0].addFields([{
-						name: `${tweet.quoted_status.user.name} (@${tweet.quoted_status.user.screen_name})`,
-						value: `>>> ${parseTweet(tweet.quoted_status)}`,
-						inline: false
-					}])
-				
-				if(tweet.favorite_count >= 100)
-					embeds[0].addFields([{
-						name: "Likes",
-						value: tweet.favorite_count.toString(),
-						inline: true
-					}])
-				if(tweet.retweet_count >= 100)
-					embeds[0].addFields([{
-						name: "Retweets",
-						value: tweet.retweet_count.toString(),
-						inline: true
-				}])
-
-				if(tweet.extended_entities) {
-					if(tweet.extended_entities.media[0].type == "photo") {
-						 embeds[0].setImage(tweet.extended_entities.media[0].media_url_https);
-
-						 if(tweet.extended_entities.media.length > 1) {
-							for(let i = 1; i < tweet.extended_entities.media.length; i++) {
-								embeds.push(new EmbedBuilder({
-									url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-									image: {url: tweet.extended_entities.media[i].media_url_https}
-								}))
-							}
-						}
-					}
-
-					if(tweet.extended_entities.media[0].type == "video" || tweet.extended_entities.media[0].type == "animated_gif") {
-						var best_video_index = -1;
-						var best_bitrate = -1;
-						for(let i = 0; i < tweet.extended_entities.media[0].video_info.variants.length; i++) {
-							var video = tweet.extended_entities.media[0].video_info.variants[i];
-							if(video.bitrate <= best_bitrate || video.content_type != "video/mp4")
-								continue;
-					
-							best_bitrate = video.bitrate;
-							best_video_index = i;
-						}
-						
-						var tweetText = parseTweet(tweet).replaceAll(/https?:\/\/[^ \[\](){}]+/g,"<$&>");
-
-						//embeds[0].setThumbnail(tweet.extended_entities.media[0].media_url_https);
-						embeds = [];
-						content = `**[${tweet.user.name} (@${tweet.user.screen_name})](<${content}>)**` +
-						` [\[video\]](${tweet.extended_entities.media[0].video_info.variants[best_video_index].url})\n` +
-						`${/\S/g.test(tweetText) ? `>>> ${tweetText}` : ""}\n`;
-					}
-				}
-				interaction.reply({content: content, embeds: embeds})
+		fetch(`https://api.fxtwitter.com/status/${tweetId}`, {
+			headers: {
+				'User-Agent': `${name}/${version} (+${contact.project_url})`
 			}
+		})
+		.then(res => {
+			return res.json()
+		})
+		.then((response) => {
+			if(response.code !== 200) {
+				interaction.reply({content: response.message, ephemeral: true});
+				return;
+			}
+
+			function parseTweet(text) {
+				var newText = text;
+				// escape encoded html
+				newText = newText.replaceAll("&lt;","<");
+				newText = newText.replaceAll("&gt;",">");
+				newText = newText.replaceAll("&amp;","&");
+				// parse @s and hashtags as links
+				newText = newText.replaceAll(/@([a-z0-9_]{1,15})/gi,"[@\u200A$1](<https://twitter.com/$1>)");
+				newText = newText.replaceAll(/#(\S+)/gi,"[#$1](<https://twitter.com/hashtag/$1>)");
+				newText = newText.replaceAll(/\$([a-z]{1,6}(?:\.[a-z]{1,2})?)/gi,"[$$$1](<https://twitter.com/search?q=%24$1&src=cashtag_click>)");
+				return newText;
+			}
+
+			var tweet = response.tweet;
+			var content = tweet.url;
+
+			var embeds = [
+				new EmbedBuilder({
+					author: {
+						name: `${tweet.author.name} (@${tweet.author.screen_name})`,
+						url: `https://twitter.com/${tweet.author.screen_name}`,
+						iconURL: tweet.author.avatar_url
+					},
+					url: tweet.url,
+					color: parseInt(tweet.color.replace("#","0x")),
+					description: parseTweet(tweet.text),
+					footer: {
+						text: tweet.source,
+						iconURL: "https://abs.twimg.com/icons/apple-touch-icon-192x192.png"
+					},
+					timestamp: new Date(tweet.created_at).toISOString()
+				})
+			];
+
+			if(tweet.quote)
+				embeds[0].addFields([{
+					name: `${tweet.quote.author.name} (@${tweet.quote.author.screen_name})`,
+					value: `>>> ${parseTweet(tweet.quote.text)}`,
+					inline: false
+				}])
+			
+			if(tweet.likes >= 100)
+				embeds[0].addFields([{
+					name: "Likes",
+					value: tweet.likes.toString(),
+					inline: true
+				}])
+			if(tweet.retweets >= 100)
+				embeds[0].addFields([{
+					name: "Retweets",
+					value: tweet.retweets.toString(),
+					inline: true
+			}])
+
+			if(tweet.poll) {
+				poll_array = [];
+
+				// very old polls don't have voters_count set, use votes_count in that case
+				// voters_count is used for multiple choice percentage calculation
+				let votes = (tweet.poll.voters_count !== null ? tweet.poll.voters_count : tweet.poll.votes_count);
+				for(let i=0; i < tweet.poll.options.length; i++) {
+					// add each poll option to an array, which will later be joined by a newline
+					poll_array.push(`${"\u2588".repeat(Math.round((tweet.poll.options[i].votes_count / votes) * 32))}\n` +
+					`${tweet.poll.options[i].title}\u2000\u2000(${tweet.poll.options[i].votes_count == 0 ? "0" : Math.round((tweet.poll.options[i].votes_count / votes) * 1000)/10}%)`)
+				}
+
+				embeds[0].addFields([{
+					name: `Poll \xB7 ${votes} votes`,
+					value: `**Close${tweet.poll.expired ? "d" : "s"} <t:${Math.round(new Date(tweet.poll.expires_at).getTime() / 1000)}:R>**\n\n` +
+					poll_array.join("\n"),
+					inline: false
+				}])
+			}
+
+			if(tweet.media || (tweet.quote && tweet.quote.media)) {
+				var mediaSource = ((tweet.quote && tweet.quote.media) && !tweet.media  ? tweet.quote.media : tweet.media);
+
+				if(mediaSource.photos) {
+					 embeds[0].setImage(mediaSource.photos[0].url);
+
+					 if(mediaSource.photos.length > 1) {
+						for(let i = 1; i < mediaSource.photos.length; i++) {
+							embeds.push(new EmbedBuilder({
+								url: tweet.url,
+								image: {url: mediaSource.photos[i].url}
+							}))
+						}
+					}
+				}
+
+				if(mediaSource.videos) {
+					media = [];
+					for(let i = 0; i < mediaSource.videos.length; i++) {
+						media.push(`[video](${mediaSource.videos[i].url})`);
+					}
+
+					embeds = [];
+					content = `**[${tweet.author.name} (@${tweet.author.screen_name})](<${tweet.url}>)**` +
+					` \[ ${media.join(" ")} \]\n` +
+					`${/\S/g.test(tweet.text) ? `>>> ${tweet.text.replace(/\[([^\[\]()]+)\]\(([^\[\]()]+)\)/g,"[$1](<$2>)")}` : ""}\n`;
+				}
+			}
+			interaction.reply({content: content, embeds: embeds})
+		})
+		.catch(err => {
+			interaction.reply({content: "Failed to fetch Tweet: ```js\n" + err + "```", ephemeral: true})
+			console.error("[Command Error: /twitter]",err);
 		});
 	}
 };
