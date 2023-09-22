bdd8db3da0b9df2ff5c54fe42c4f2ffe3abd4bad
Erika <50500602+Enbika@users.noreply.github.com>
2023-09-22 09:57:56 -0600

    defer mastodon interaction for slow instances

--- commands/mastodon.js
+++ commands/mastodon.js
@@ -41,119 +41,122 @@ exports.module = {
 			return;
 		}
 		
-		fetch(`https://${regex.exec(interaction.options.getString('url'))[1]}/api/v1/statuses/${regex.exec(interaction.options.getString('url'))[2]}`, {
-			headers: {
-				'User-Agent': `${name}/${version} (+${contact.project_url})`
-			}
-		})
-		.then(res => {
-			return res.json()
-		})
-		.then((toot) => {
-			if(toot.error) {
-				interaction.reply({content: toot.error, ephemeral: true});
-				return;
-			}
-			var showCW = interaction.options.getBoolean('cw');
-			var cwText =  (toot.spoiler_text !== "" ? (toot.spoiler_text > 500 ? toot.spoiler_text.substr(0,499) + "…" : toot.spoiler_text) : "")
-			var content = (toot.spoiler_text !== "" ? `**CW: ${cwText}** ${showCW ? toot.url : `||${toot.url}||`}` : toot.url);
-			var tootText = turndownService.turndown(toot.content).replace(/\[([^[\]()]+)]\(\1\)/g,"<$1>");
+		interaction.deferReply().then(response => {
+			interaction = response.interaction;
+			fetch(`https://${regex.exec(interaction.options.getString('url'))[1]}/api/v1/statuses/${regex.exec(interaction.options.getString('url'))[2]}`, {
+				headers: {
+					'User-Agent': `${name}/${version} (+${contact.project_url})`
+				}
+			})
+			.then(res => {
+				return res.json()
+			})
+			.then((toot) => {
+				if(toot.error) {
+					response.edit({content: toot.error});
+					return;
+				}
+				var showCW = interaction.options.getBoolean('cw');
+				var cwText =  (toot.spoiler_text !== "" ? (toot.spoiler_text > 500 ? toot.spoiler_text.substr(0,499) + "…" : toot.spoiler_text) : "")
+				var content = (toot.spoiler_text !== "" ? `**CW: ${cwText}** ${showCW ? toot.url : `||${toot.url}||`}` : toot.url);
+				var tootText = turndownService.turndown(toot.content).replace(/\[([^[\]()]+)]\(\1\)/g,"<$1>");
 
-			var embeds = [
-				new EmbedBuilder({
-					author: {
-						name: `${toot.account.display_name} (@${toot.account.acct})`,
-						url: toot.account.url,
-						iconURL: toot.account.avatar
-					},
-					url: toot.url,
-					color: toot.sensitive ? 0xf4212e : 0x00ba7c,
-					description: (tootText.length > 4096 ? tootText.substr(0,4095) + "…" : tootText),
-					footer: {
-						text: (toot.application ? toot.application.name : new URL(toot.url).hostname),
-						iconURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mastodon_logotype_%28simple%29_new_hue.svg/128px-Mastodon_logotype_%28simple%29_new_hue.svg.png"
-					},
-					timestamp: new Date(toot.created_at).toISOString()
-				})
-			];
-			
-			if(toot.edited_at !== null)
-				embeds[0].addFields([{
-					name: "Last edited",
-					value: `<t:${Math.round(new Date(toot.edited_at).getTime() / 1000)}>`,
-					inline: true
+				var embeds = [
+					new EmbedBuilder({
+						author: {
+							name: `${toot.account.display_name} (@${toot.account.acct})`,
+							url: toot.account.url,
+							iconURL: toot.account.avatar
+						},
+						url: toot.url,
+						color: toot.sensitive ? 0xf4212e : 0x00ba7c,
+						description: (tootText.length > 4096 ? tootText.substr(0,4095) + "…" : tootText),
+						footer: {
+							text: (toot.application ? toot.application.name : new URL(toot.url).hostname),
+							iconURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mastodon_logotype_%28simple%29_new_hue.svg/128px-Mastodon_logotype_%28simple%29_new_hue.svg.png"
+						},
+						timestamp: new Date(toot.created_at).toISOString()
+					})
+				];
+				
+				if(toot.edited_at !== null)
+					embeds[0].addFields([{
+						name: "Last edited",
+						value: `<t:${Math.round(new Date(toot.edited_at).getTime() / 1000)}>`,
+						inline: true
+					}])
+				
+				if(toot.favourites_count >= 100)
+					embeds[0].addFields([{
+						name: "Favourites",
+						value: toot.favourites_count.toString(),
+						inline: true
+					}])
+				if(toot.reblogs_count >= 100)
+					embeds[0].addFields([{
+						name: "Boosts",
+						value: toot.reblogs_count.toString(),
+						inline: true
 				}])
-			
-			if(toot.favourites_count >= 100)
-				embeds[0].addFields([{
-					name: "Favourites",
-					value: toot.favourites_count.toString(),
-					inline: true
-				}])
-			if(toot.reblogs_count >= 100)
-				embeds[0].addFields([{
-					name: "Boosts",
-					value: toot.reblogs_count.toString(),
-					inline: true
-			}])
 
-			if(toot.poll) {
-				poll_array = [];
+				if(toot.poll) {
+					poll_array = [];
 
-				// very old polls don't have voters_count set, use votes_count in that case
-				// voters_count is used for multiple choice percentage calculation
-				let votes = (toot.poll.voters_count !== null ? toot.poll.voters_count : toot.poll.votes_count);
-				for(let i=0; i < toot.poll.options.length; i++) {
-					// add each poll option to an array, which will later be joined by a newline
-					poll_array.push(`${"\u2588".repeat(Math.round((toot.poll.options[i].votes_count / votes) * 32))}\n` +
-					`${toot.poll.options[i].title}\u2000\u2000(${toot.poll.options[i].votes_count == 0 ? "0" : Math.round((toot.poll.options[i].votes_count / votes) * 1000)/10}%)`)
+					// very old polls don't have voters_count set, use votes_count in that case
+					// voters_count is used for multiple choice percentage calculation
+					let votes = (toot.poll.voters_count !== null ? toot.poll.voters_count : toot.poll.votes_count);
+					for(let i=0; i < toot.poll.options.length; i++) {
+						// add each poll option to an array, which will later be joined by a newline
+						poll_array.push(`${"\u2588".repeat(Math.round((toot.poll.options[i].votes_count / votes) * 32))}\n` +
+						`${toot.poll.options[i].title}\u2000\u2000(${toot.poll.options[i].votes_count == 0 ? "0" : Math.round((toot.poll.options[i].votes_count / votes) * 1000)/10}%)`)
+					}
+
+					embeds[0].addFields([{
+						name: `Poll \xB7 ${votes} votes`,
+						value: `**Close${toot.poll.expired ? "d" : "s"} <t:${Math.round(new Date(toot.poll.expires_at).getTime() / 1000)}:R>**\n\n` +
+						poll_array.join("\n"),
+						inline: false
+					}])
 				}
 
-				embeds[0].addFields([{
-					name: `Poll \xB7 ${votes} votes`,
-					value: `**Close${toot.poll.expired ? "d" : "s"} <t:${Math.round(new Date(toot.poll.expires_at).getTime() / 1000)}:R>**\n\n` +
-					poll_array.join("\n"),
-					inline: false
-				}])
-			}
+				if(toot.media_attachments.length > 0) {
+					if(toot.media_attachments[0].type == "image") {
+						embeds[0].setImage(toot.media_attachments[0].url);
 
-			if(toot.media_attachments.length > 0) {
-				if(toot.media_attachments[0].type == "image") {
-					 embeds[0].setImage(toot.media_attachments[0].url);
-
-					 if(toot.media_attachments.length > 1) {
-						for(let i = 1; i < toot.media_attachments.length; i++) {
-							embeds.push(new EmbedBuilder({
-								url: toot.url,
-								image: {url: toot.media_attachments[i].url}
-							}))
+						if(toot.media_attachments.length > 1) {
+							for(let i = 1; i < toot.media_attachments.length; i++) {
+								embeds.push(new EmbedBuilder({
+									url: toot.url,
+									image: {url: toot.media_attachments[i].url}
+								}))
+							}
 						}
 					}
-				}
 
-				var hasVideo = false;
-				for(let j=0; j<toot.media_attachments.length; j++) {
-					if(toot.media_attachments[j].type == "video" || toot.media_attachments[j].type == "gifv" || toot.media_attachments[j].type == "audio")
-						hasVideo = true;
-				}
-				if(hasVideo) {
-					media = [];
-					for(let i = 0; i < toot.media_attachments.length; i++) {
-						media.push(`[${toot.media_attachments[i].type}](${toot.media_attachments[i].url})`);
+					var hasVideo = false;
+					for(let j=0; j<toot.media_attachments.length; j++) {
+						if(toot.media_attachments[j].type == "video" || toot.media_attachments[j].type == "gifv" || toot.media_attachments[j].type == "audio")
+							hasVideo = true;
 					}
+					if(hasVideo) {
+						media = [];
+						for(let i = 0; i < toot.media_attachments.length; i++) {
+							media.push(`[${toot.media_attachments[i].type}](${toot.media_attachments[i].url})`);
+						}
 
-					embeds = [];
-					content = (toot.spoiler_text !== "" ? `**CW: ${cwText}**\n${showCW ? "" : "||"}` : "") + 
-					`**[${toot.account.display_name} (@${toot.account.acct})](<${toot.url}>)**` +
-					` \[ ${media.join(" ")} \]\n` +
-					`${/\S/g.test(tootText) ? `>>> ${(tootText.length > 1024 ? tootText.substr(0,1023) + "…" : tootText).replace(/\[([^\[\]()]+)\]\(([^\[\]()]+)\)/g,"[$1](<$2>)")}` : ""}${toot.spoiler_text !== "" && !showCW ? "||" : ""}\n`;
+						embeds = [];
+						content = (toot.spoiler_text !== "" ? `**CW: ${cwText}**\n${showCW ? "" : "||"}` : "") + 
+						`**[${toot.account.display_name} (@${toot.account.acct})](<${toot.url}>)**` +
+						` \[ ${media.join(" ")} \]\n` +
+						`${/\S/g.test(tootText) ? `>>> ${(tootText.length > 1024 ? tootText.substr(0,1023) + "…" : tootText).replace(/\[([^\[\]()]+)\]\(([^\[\]()]+)\)/g,"[$1](<$2>)")}` : ""}${toot.spoiler_text !== "" && !showCW ? "||" : ""}\n`;
+					}
 				}
-			}
-			interaction.reply({content: content, embeds: embeds})
+				response.edit({content: content, embeds: embeds})
+			})
+			.catch(err => {
+				response.edit({content: "Failed to fetch Mastodon post: ```js\n" + err + "```"})
+				console.error("[Command Error: /mastodon]",err);
+			});
 		})
-		.catch(err => {
-			interaction.reply({content: "Failed to fetch Mastodon post: ```js\n" + err + "```", ephemeral: true})
-			console.error("[Command Error: /mastodon]",err);
-		});
 	}
 };
